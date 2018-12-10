32b9df6b7c2991242504cf022ccce9329f747d47
craigbranscom <craig.branscom@gmail.com>
2018-12-10 11:18:30 -0800

    Update telos.rb

--- telos.rb
+++ telos.rb
@@ -10,12 +10,12 @@ class Telos < Formula
     depends_on "gmp" 
     depends_on "gettext"
     depends_on "openssl"
-    depends_on :macos => :high_sierra
+    #depends_on :macos => :high_sierra
     depends_on :arch =>  :intel
    
     bottle do
         root_url "https://github.com/Telos-Foundation/telos/releases/download/v1.0.0"
-        sha256 "" => :high_sierra
+        #sha256 "" => :high_sierra
         sha256 "" => :mojave
     end
         
